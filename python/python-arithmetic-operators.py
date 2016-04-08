4bf4cffa9bf1f6cdf431aa09f52272ff55589c49
伟周 <zhouwei@igola.com>
2016-04-08 19:32:05 +0800

    ppppp

--- python/python-arithmetic-operators.py
+++ python/python-arithmetic-operators.py
@@ -1,2 +1,6 @@
 # https://www.hackerrank.com/challenges/python-arithmetic-operators
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+a, b = int(raw_input()), int(raw_input())
+print(a+b)
+print(a-b)
+print(a*b)
