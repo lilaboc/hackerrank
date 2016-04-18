09d01deaa1749ba3f12a457242706a78ccc4c246
伟周 <zhouwei@igola.com>
2016-04-18 18:42:16 +0800

    Apr 18

---
+++ ruby/ruby-array-addition.rb
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/ruby-array-addition
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def end_arr_add(arr, element)
+    # Add `element` to the end of the Array variable `arr` and return `arr`
+    arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+    # Add `element` to the beginning of the Array variable `arr` and return `arr`
+    arr.insert(0, element)
+end
+
+def index_arr_add(arr, index, element)
+    # Add `element` at position `index` to the Array variable `arr` and return `arr`
+    arr.insert(index, element)
+end
+
+def index_arr_multiple_add(arr, index)
+    # add any two elements to the arr at the index
+    arr.insert(index, 'strange', 'task')
+end
+
